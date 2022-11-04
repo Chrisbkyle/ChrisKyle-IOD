7ff2f1421399b9e5a8aef7127d82169e61577643
Chris <christopher.b.kyle@gmail.com>
2022-11-05 11:59:11 +1300

    test

---
+++ 11.05.22-Lab-2/10.22.22-Lab-1/activity2.js
@@ -0,0 +1,9 @@
+const book =  {
+    "title" : "Scar Tissue",
+    "author" :  "Anthony Kiedis",
+    "description" : "Autobiography",
+    "published" : "2004",
+    "length" : "460 pages"
+}
+
+console.log(book);
